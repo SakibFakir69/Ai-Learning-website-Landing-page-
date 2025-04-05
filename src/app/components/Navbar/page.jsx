0aa2b056bfeb0f4202ffec8c93099fcea0f02fd4
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-05 13:03:30 +0600

    working on navbar page this website

---
+++ src/app/components/Navbar/page.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+import React from 'react'
+// here implements navbar 
+function NavbarPage() {
+    // home about service contact plan contact use 
+
+
+  return (
+    <div>
+        {/* link use to navagiate  */}
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default page
