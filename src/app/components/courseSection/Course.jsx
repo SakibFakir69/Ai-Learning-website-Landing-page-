00508195d8d09c79a5f34d6ec10d81cf5a35d639
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-10 13:35:18 +0600

    course all section add done

--- src/app/components/courseSection/Course.jsx
+++ src/app/components/courseSection/Course.jsx
@@ -5,6 +5,9 @@
 import React from 'react'
 import Image from 'next/image'
 import image1 from './../../../../public./Rectangle 9.png'
+import image2 from './../../../../public./Rectangle 10.png'
+import image3 from './../../../../public./Rectangle 13.png'
+import image4 from './../../../../public./Rectangle 12.png'
 
 function Course() {
   return (
@@ -14,21 +17,51 @@ function Course() {
 
             <div>
 
-                <Image src={image1} height={200} alt="image"/>
+                <Image src={image1} height={200} alt="image"
+                className='rounded'/>
 
                 <div>
                     {/* icon */}
                     <h3 className='text-white font-semibold'>Machine Learning</h3>
-                    
-                    <p>In publishing and graphic design, Lorem ipsum is a placeholder. </p>
+
+                    <p className='text-gray-300'>In publishing and graphic design, Lorem ipsum is a placeholder. </p>
                 </div>
 
 
             </div>
 
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+                <Image src={image2} height={200} alt='image2' className='rounded'/>
+
+                <div>
+                    <h3 className='text-white font-semibold'>Machine Learning</h3>
+                    <p className='text-gray-300'>In publishing and graphic design, Lorem ipsum is a placeholder. </p>
+                </div>
+
+
+            </div>
+
+
+            <div>
+                <Image src={image3} height={200} alt='image3'/>
+
+                <div>
+                    <h3 className='text-white font-semibold'>Machine Learning</h3>
+                    <p className='text-gray-300'>In publishing and graphic design, Lorem ipsum is a placeholder. </p>
+
+                </div>
+
+
+            </div>
+
+
+            <div>
+                <Image src={image4} height={200} alt='image4'/>
+                <div>
+                    <h3 className='text-white'>Machine Learning</h3>
+                    <p className='text-gray-300'>In publishing and graphic design, Lorem ipsum is a placeholder. </p>
+                </div>
+            </div>
         </section>
 
 
