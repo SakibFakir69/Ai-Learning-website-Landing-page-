f4479820f0a46390febc72633819409de7cb3218
Sakib Fakir <FakirSakib22232@gmail.com>
2025-04-11 11:47:34 +0600

    number section done

--- src/app/components/numberSection/Number.jsx
+++ src/app/components/numberSection/Number.jsx
@@ -5,9 +5,10 @@ import image1 from "./../../../../public/Rectangle 15.png";
 function Number() {
   return (
     <div className="w-full all-color ">
-      <section className="flex justify-center mt-6 w-full border p-4 md:gap-28 gap-5 md:flex-row flex-col items-center">
+      <section className="flex justify-center mt-6 w-full border p-4 md:gap-28 gap-5 md:flex-row flex-col items-center md:h-52 ">
 
-        <div className="h-28 md:w-48 w-full  border flex text-center flex-col justify-center">
+        <div className="h-28 md:w-48 w-full   flex text-center flex-col justify-center border-2 border-red-500 border-b-cyan-700 rounded-tl-4xl rounded-br-4xl">
+
           <h3 className="md:text-2xl text-xl font-semibold text-green-500">
             250+
           </h3>
@@ -15,7 +16,9 @@ function Number() {
         </div>
         {/* 2 */}
 
-        <div className="h-28 md:w-48 w-full  border flex text-center flex-col justify-center">
+        <div className="h-28 md:w-48 w-full  flex text-center flex-col justify-center 
+        
+      border-2 border-blue-600 rounded-tl-4xl border-b-red-600 rounded-br-4xl">
           <h3 className="md:text-2xl text-xl font-semibold text-green-500">
             250+
           </h3>
@@ -23,7 +26,8 @@ function Number() {
         </div>
         {/* 3 */}
 
-        <div className="h-28 md:w-48 w-full  border flex text-center flex-col justify-center">
+        <div className="h-28 md:w-48 w-full  border-2 flex text-center flex-col justify-center border-emerald-500 rounded-tl-4xl rounded-br-4xl border-t-emerald-900">
+
           <h3 className="md:text-2xl text-xl font-semibold text-green-500">
             250+
           </h3>
@@ -31,7 +35,7 @@ function Number() {
         </div>
         {/* 4 */}
 
-        <div className="h-28 md:w-48 w-full  border flex text-center flex-col justify-center">
+        <div className="h-28 md:w-48 w-full  border-2 flex text-center flex-col justify-center border-white rounded-tl-4xl rounded-br-4xl border-t-amber-300">
           <h3 className="md:text-2xl text-xl font-semibold text-green-500">
             250+
           </h3>
